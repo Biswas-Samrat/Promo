8f16cf6234034837333d440674978ca57ef158bd
Samrat Biswas <samrat.tx@gmail.com>
2025-07-07 05:22:50 +0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./landing_page/home/HomePage";
-import Abaout from "./landing_Page/abaout/abaoutPage";
-import Contact from "./landing_Page/contact/contactPage";
-import Features from "./landing_Page/features/featurePage";
+import HomePage from "./landing_page/home/HomePage"; 
+import Abaout from "./landing_page/abaout/abaoutPage"; 
+import Contact from "./landing_page/contact/contactPage"; 
+import Features from "./landing_page/features/featurePage"; 
 import JoinPage from "./landing_page/creatAccaount/Join"
 import Login from "./landing_page/login/Login"
 import Nav from "./landing_page/navbar"
 
 
-
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-  <Nav />
-    <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/Abaout" element={<Abaout />}></Route>
-      <Route path="/Contact" element={<Contact />}></Route>
-      <Route path="/Features" element={<Features />}></Route>
-       <Route path="/JoinPage" element={<JoinPage />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
-   
-    </Routes>
-  </BrowserRouter>
+  <BrowserRouter>
+  <Nav />
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/Abaout" element={<Abaout />}></Route>
+      <Route path="/Contact" element={<Contact />}></Route>
+      <Route path="/Features" element={<Features />}></Route>
+       <Route path="/JoinPage" element={<JoinPage />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+   
+    </Routes>
+  </BrowserRouter>
 );
