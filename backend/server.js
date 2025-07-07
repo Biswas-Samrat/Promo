8e1974910bc0c40b286900dacb1f0c34857d8b59
Samrat Biswas <samrat.tx@gmail.com>
2025-07-07 06:41:13 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ cloudinary.config({
 // Middleware Setup
 // Middleware Setup
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"], 
+    origin: ["https://promo-1-v4b5.onrender.com", "https://promo-2-ocwm.onrender.com", "https://promo-3.onrender.com"], 
     credentials: true
 }));
 
